8fb95862de047c51bdf23380440b0f781064734e
Shannon Lewis <slewis74@gmail.com>
2019-07-01 19:08:38 +1000

    added overwrite-mode to the push and push metadata commands

--- source/Octopus.Cli/Commands/Package/PushMetadataCommand.cs
+++ source/Octopus.Cli/Commands/Package/PushMetadataCommand.cs
@@ -5,6 +5,7 @@ using Octopus.Cli.Infrastructure;
 using Octopus.Cli.Repositories;
 using Octopus.Cli.Util;
 using Octopus.Client;
+using Octopus.Client.Model;
 using Octopus.Client.Model.PackageMetadata;
 
 namespace Octopus.Cli.Commands.Package
@@ -21,13 +22,14 @@ namespace Octopus.Cli.Commands.Package
             options.Add("package-id=", "The ID of the package; e.g. MyCompany.MyApp", v => PackageId = v);
             options.Add("version=", "The version of the package; defaults to a timestamp-based version", v => Version = v);
             options.Add("metadata-file=", "Octopus Package metadata Json file.", file => MetadataFile = file);
-            options.Add("replace-existing", "If the package metadata already exists in the repository, the default behavior is to reject the new package metadata being pushed. You can pass this flag to overwrite the existing package.", replace => ReplaceExisting = true);
+            options.Add("overwrite-mode=", "If the package metadata already exists in the repository, the default behavior is to reject the new package metadata being pushed (FailIfExists). You can use the overwrite mode to OverwriteExisting or IgnoreIfExists.", mode => OverwriteMode = (OverwriteMode)Enum.Parse(typeof(OverwriteMode), mode));
+            options.Add("replace-existing", "If the package metadata already exists in the repository, the default behavior is to reject the new package metadata being pushed. You can pass this flag to overwrite the existing package. This flag may be deprecated in a future version, passing it is the same as using the OverwriteExisting overwrite-mode.", replace => OverwriteMode = OverwriteMode.OverwriteExisting);
         }
 
         public string PackageId { get; set; }
         public string Version { get; set; }
         public string MetadataFile { get; set; }
-        public bool ReplaceExisting { get; set; }
+        public OverwriteMode OverwriteMode { get; set; }
 
         public async Task Request()
         {
@@ -39,7 +41,7 @@ namespace Octopus.Cli.Commands.Package
             var fileContent = FileSystem.ReadAllText(MetadataFile);
             var octopusPackageMetadata = JsonConvert.DeserializeObject<OctopusPackageMetadata>(fileContent);
 
-            resultResource = await Repository.PackageMetadataRepository.Push(PackageId, Version, octopusPackageMetadata, ReplaceExisting);
+            resultResource = await Repository.PackageMetadataRepository.Push(PackageId, Version, octopusPackageMetadata, OverwriteMode);
         }
 
         public void PrintDefaultOutput()
