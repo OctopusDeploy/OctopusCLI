0d2d1e1fb66d0df8e6e752e3f126a597faafcce2
David Young <thedewi@users.noreply.github.com>
2020-02-19 10:16:38 +1000

    Apply suggestions from code review
    Adjust warning messages to say "Deprecated"
    
    Co-Authored-By: Matt Richardson <matt.richardson@octopus.com>

--- BuildAssets/OctoWrapper.sh
+++ BuildAssets/OctoWrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo 'Note: The Octo command has been renamed to octo. Redirecting...' >&2
+echo 'Deprecated: The Octo command has been renamed to octo.' >&2
 "$(dirname "$0")/octo" "$@"
