1ac3dc82c346b5ae727119b7b412a64d132c7437
David Young <thedewi@users.noreply.github.com>
2020-02-21 18:22:54 +1000

    Update publish-rpm.sh for octopuscli

--- BuildAssets/repos/publish-rpm.sh
+++ BuildAssets/repos/publish-rpm.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-#Usage: ./publish-rpm.sh -s artifacts -t octopus-rpm-repo
-#artifacts: this is the directory containing the rpm file(s)
+# Required env vars:
+#export AWS_ACCESS_KEY_ID=$(get_octopusvariable "OctopusToolsAwsAccount.AccessKey")
+#export AWS_SECRET_ACCESS_KEY=$(get_octopusvariable "OctopusToolsAwsAccount.SecretKey")
+#export S3_PUBLISH_ENDPOINT=$(get_octopusvariable "Publish.APT.S3.TargetBucket")
 
-# Publishes built RPMs to an s3-backed RPM repo.
-export AWS_ACCESS_KEY_ID=$(get_octopusvariable "Publish.RPM.S3.AccessKeyId")
-export AWS_SECRET_ACCESS_KEY=$(get_octopusvariable "Publish.RPM.S3.SecretAccessKey")
-
-set -e
 if [ ! -z "${DEBUG}" ]; then
   set -x
 fi
 
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-SRC_BASE="${SCRIPT_DIR}/../.."
-
 DEPENDENCIES=("aws" "createrepo")
-SOURCE_DIR=""
-TARGET_BUCKET=""
-
 for dep in "${DEPENDENCIES[@]}"
 do
   if [ ! $(which ${dep}) ]; then
@@ -27,45 +18,28 @@ do
   fi
 done
 
-while getopts "s:t:" opt; do
-  case $opt in
-    s) SOURCE_DIR=$OPTARG ;;
-    t) TARGET_BUCKET=$OPTARG ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
-done
+echo "Configuring S3 bucket"
+#aws s3 mb "s3://${S3_PUBLISH_ENDPOINT}"|| echo ERROR
+#aws s3api wait bucket-exists --bucket ${S3_PUBLISH_ENDPOINT}|| echo ERROR
+#aws s3 sync ./rpm-content "s3://${S3_PUBLISH_ENDPOINT}" --acl public-read|| echo ERROR
 
-if [ -z "${SOURCE_DIR}" ]; then
-  echo "Source directory must be specified."
-  exit 1
-fi
-
-if [ -z "${TARGET_BUCKET}" ]; then
-  echo "Target bucket must be specified."
-  exit 1
-fi
-
-aws s3 mb "s3://${TARGET_BUCKET}"
-aws s3api wait bucket-exists --bucket ${TARGET_BUCKET}
-aws s3 sync ./rpm-content "s3://${TARGET_BUCKET}" --acl public-read
-
-TARGET_DIR="/tmp/${TARGET_BUCKET}"
+TARGET_DIR="/tmp/$S3_PUBLISH_ENDPOINT"
 
 # make sure we're operating on the latest data in the target bucket
-mkdir -p $TARGET_DIR
-aws s3 sync "s3://${TARGET_BUCKET}" $TARGET_DIR
+rm -rf "$TARGET_DIR"|| exit 1
+mkdir -p "$TARGET_DIR"|| exit 1
+aws s3 sync "s3://$S3_PUBLISH_ENDPOINT" "$TARGET_DIR"|| exit 1
 
 # copy the RPM in and update the repo
-mkdir -pv $TARGET_DIR/x86_64/
-cp -rv $SOURCE_DIR/*.rpm $TARGET_DIR/x86_64/
+mkdir -pv "$TARGET_DIR/x86_64/"|| exit 1
+cp -v OctopusTools.Packages.linux-x64/*.rpm "$TARGET_DIR/x86_64/"|| exit 1
 UPDATE=""
 if [ -e "$TARGET_DIR/x86_64/repodata/repomd.xml" ]; then
   UPDATE="--update "
 fi
-for a in $TARGET_DIR/x86_64 ; do createrepo -v $UPDATE --deltas $a/ ; done
+for a in "$TARGET_DIR/x86_64"; do
+  createrepo -v $UPDATE --deltas "$a/"|| exit 1
+done
 
 # sync the repo state back to s3
-aws s3 sync $TARGET_DIR s3://$TARGET_BUCKET --acl public-read
+aws s3 sync "$TARGET_DIR" "s3://$S3_PUBLISH_ENDPOINT" --acl public-read --delete|| exit 1
