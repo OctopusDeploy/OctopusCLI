c3f3efb81b75a72da46598aafe5099c92a46a0b9
Andy Hsu <andy.hsu@octopus.com>
2020-03-06 16:46:09 +1000

    Added PreventReleaseProgressionCommand and its unit tests

---
+++ source/Octo.Tests/Commands/PreventReleaseProgressionCommandFixture.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Octopus.Cli.Commands;
+using Octopus.Cli.Commands.Releases;
+using Octopus.Cli.Infrastructure;
+using Octopus.Client.Exceptions;
+using Octopus.Client.Model;
+using Octopus.Client.Serialization;
+
+namespace Octo.Tests.Commands
+{
+    [TestFixture]
+    public class PreventReleaseProgressionCommandFixture : ApiCommandFixtureBase
+    {
+        PreventReleaseProgressionCommand preventReleaseProgressionCommand;
+        ProjectResource projectResource;
+        ReleaseResource releaseResource;
+        const string ReasonToPrevent = "Test Prevention";
+
+        [SetUp]
+        public void SetUp()
+        {
+            preventReleaseProgressionCommand = new PreventReleaseProgressionCommand(ClientFactory, RepositoryFactory, FileSystem, CommandOutputProvider);
+
+            projectResource = new ProjectResource
+            {
+                Id = "Projects-1",
+                Name = "Test Project",
+                SpaceId = "Spaces-1"
+            };
+            Repository.Projects.FindByName(projectResource.Name).Returns(projectResource);
+
+            releaseResource = new ReleaseResource
+            {
+                Id = "Releases-1",
+                ProjectId = projectResource.Id,
+                SpaceId = projectResource.SpaceId,
+                Version = "0.0.1"
+            };
+            Repository.Projects.GetReleaseByVersion(projectResource, releaseResource.Version).Returns(releaseResource);
+        }
+
+        [Test]
+        public void ShouldBeSubClassOfCorrectBaseClass()
+        {
+            typeof(PreventReleaseProgressionCommand).IsSubclassOf(typeof(ApiCommand)).Should().BeTrue();
+        }
+
+        [Test]
+        public void ShouldImplementCorrectInterface()
+        {
+            typeof(ISupportFormattedOutput).IsAssignableFrom(typeof(PreventReleaseProgressionCommand)).Should().BeTrue();
+        }
+
+        [Test]
+        public void ShouldBeAttachedWithCorrectAttribute()
+        {
+            var attribute = typeof(PreventReleaseProgressionCommand).GetCustomAttribute<CommandAttribute>();
+
+            attribute.Should().NotBeNull();
+
+            attribute.Name.Should().Be("prevent-release-progression");
+        }
+
+        [Test]
+        public void ShouldValidateProjectIdOrNameParameter()
+        {
+            CommandLineArgs.Add($"--version={releaseResource.Version}");
+            CommandLineArgs.Add($"--reason={ReasonToPrevent}");
+
+            Func<Task> exec = () => preventReleaseProgressionCommand.Execute(CommandLineArgs.ToArray());
+            exec.ShouldThrow<CommandException>()
+                .WithMessage("Please specify a project name or ID using the parameter: --project=XYZ");
+        }
+
+        [Test]
+        public void ShouldValidateReasonToPreventParameter()
+        {
+            CommandLineArgs.Add($"--project={projectResource.Name}");
+            CommandLineArgs.Add($"--version={releaseResource.Version}");
+
+            Func<Task> exec = () => preventReleaseProgressionCommand.Execute(CommandLineArgs.ToArray());
+            exec.ShouldThrow<CommandException>()
+                .WithMessage("Please specify a reason why you would like to prevent this release from progressing to next phase");
+        }
+
+        [Test]
+        public void ShouldValidateReleaseVersionNumberParameter()
+        {
+            CommandLineArgs.Add($"--project={projectResource.Name}");
+            CommandLineArgs.Add($"--reason={ReasonToPrevent}");
+
+            Func<Task> exec = () => preventReleaseProgressionCommand.Execute(CommandLineArgs.ToArray());
+            exec.ShouldThrow<CommandException>()
+                .WithMessage("Please specify a release version");
+        }
+
+        [TestCase("version")]
+        [TestCase("releaseNumber")]
+        public async Task ShouldSupportBothReleaseNumberAndVersionArgForReleaseVersionNumberProperty(string argName)
+        {
+            CommandLineArgs.Add($"--project={projectResource.Name}");
+            CommandLineArgs.Add($"--{argName}={releaseResource.Version}");
+            CommandLineArgs.Add($"--reason={ReasonToPrevent}");
+
+            await preventReleaseProgressionCommand.Execute(CommandLineArgs.ToArray());
+
+            preventReleaseProgressionCommand.ReleaseVersionNumber.Should().Be(releaseResource.Version);
+            await Repository.Projects.Received(1).GetReleaseByVersion(projectResource, preventReleaseProgressionCommand.ReleaseVersionNumber);
+        }
+
+        [Test]
+        public void ShouldThrowCorrectException_WhenReleaseIssNotFound()
+        {
+            Repository.Projects.GetReleaseByVersion(projectResource, releaseResource.Version).Returns((ReleaseResource) null);
+
+            CommandLineArgs.Add($"--project={projectResource.Name}");
+            CommandLineArgs.Add($"--version={releaseResource.Version}");
+            CommandLineArgs.Add($"--reason={ReasonToPrevent}");
+
+            Func<Task> exec = () => preventReleaseProgressionCommand.Execute(CommandLineArgs.ToArray());
+
+            exec.ShouldThrow<OctopusResourceNotFoundException>();
+        }
+
+        [Test]
+        public async Task ShouldPreventReleaseProgressionCorrectly()
+        {
+            CommandLineArgs.Add($"--project={projectResource.Name}");
+            CommandLineArgs.Add($"--version={releaseResource.Version}");
+            CommandLineArgs.Add($"--reason={ReasonToPrevent}");
+
+            await preventReleaseProgressionCommand.Execute(CommandLineArgs.ToArray());
+
+            preventReleaseProgressionCommand.ProjectNameOrId.Should().Be(projectResource.Name);
+            preventReleaseProgressionCommand.ReleaseVersionNumber.Should().Be(releaseResource.Version);
+            preventReleaseProgressionCommand.ReasonToPrevent.Should().Be(ReasonToPrevent);
+
+            await Repository.Projects.Received(1).FindByName(projectResource.Name);
+            await Repository.Projects.Received(1).GetReleaseByVersion(projectResource, releaseResource.Version);
+            await Repository.Defects.Received(1).RaiseDefect(releaseResource, ReasonToPrevent);
+        }
+
+        [Test]
+        public void ShouldPrintDefaultOutputCorrectly()
+        {
+            preventReleaseProgressionCommand.PrintDefaultOutput();
+            
+            LogOutput.ToString().Trim().Should().Be("Prevented successfully.");
+        }
+
+        [Test]
+        public async Task ShouldPrintJsonOutputCorrectly()
+        {
+            CommandLineArgs.Add($"--project={projectResource.Name}");
+            CommandLineArgs.Add($"--version={releaseResource.Version}");
+            CommandLineArgs.Add($"--reason={ReasonToPrevent}");
+            CommandLineArgs.Add("--outputFormat=json");
+
+            await preventReleaseProgressionCommand.Execute(CommandLineArgs.ToArray());
+
+            var logOutput = LogOutput.ToString().Trim();
+            var expectedLogOutput = JsonSerialization.SerializeObject(new
+            {
+                projectResource.SpaceId,
+                Project = new { projectResource.Id, projectResource.Name },
+                Release = new { releaseResource.Id, releaseResource.Version, IsPreventedFromProgressing = true },
+                ReasonToPrevent
+            });
+
+            logOutput.Should().Be(expectedLogOutput);
+        }
+    }
+}
---
+++ source/Octopus.Cli/Commands/Releases/PreventReleaseProgressionCommand.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using Octopus.Cli.Infrastructure;
+using Octopus.Cli.Repositories;
+using Octopus.Cli.Util;
+using Octopus.Client;
+using Octopus.Client.Exceptions;
+using Octopus.Client.Model;
+
+namespace Octopus.Cli.Commands.Releases
+{
+    [Command("prevent-release-progression", Description = "Prevent a release from progressing to next phase.")]
+    public class PreventReleaseProgressionCommand : ApiCommand, ISupportFormattedOutput
+    {
+        ProjectResource project;
+        ReleaseResource release;
+
+        public PreventReleaseProgressionCommand(IOctopusClientFactory clientFactory, IOctopusAsyncRepositoryFactory repositoryFactory, IOctopusFileSystem fileSystem, ICommandOutputProvider commandOutputProvider)
+            : base(clientFactory, repositoryFactory, fileSystem, commandOutputProvider)
+        {
+            var options = Options.For("Preventing release progression.");
+            options.Add("project=", "Name or ID of the project", v => ProjectNameOrId = v);
+            options.Add("version=|releaseNumber=", "Release number to use for auto deployments.",
+                v => ReleaseVersionNumber = v);
+            options.Add("reason=", "Reason to prevent this release from progressing to next phase.",
+                v => ReasonToPrevent = v);
+        }
+
+        public string ProjectNameOrId { get; set; }
+
+        public string ReleaseVersionNumber { get; set; }
+
+        public string ReasonToPrevent { get; set; }
+
+        protected override async Task ValidateParameters()
+        {
+            if (string.IsNullOrWhiteSpace(ProjectNameOrId)) throw new CommandException("Please specify a project name or ID using the parameter: --project=XYZ");
+            if (string.IsNullOrWhiteSpace(ReleaseVersionNumber)) throw new CommandException("Please specify a release version");
+            if (string.IsNullOrWhiteSpace(ReasonToPrevent)) throw new CommandException("Please specify a reason why you would like to prevent this release from progressing to next phase");
+
+            await base.ValidateParameters().ConfigureAwait(false);
+        }
+
+        public async Task Request()
+        {
+            project = await Repository.Projects.FindByNameOrIdOrFail(ProjectNameOrId).ConfigureAwait(false);
+
+            release = await Repository.Projects.GetReleaseByVersion(project, ReleaseVersionNumber).ConfigureAwait(false);
+            if (release == null) throw new OctopusResourceNotFoundException($"Unable to locate a release with version/release number '${ReleaseVersionNumber}'.");
+            
+            await Repository.Defects.RaiseDefect(release, ReasonToPrevent).ConfigureAwait(false);
+        }
+
+        public void PrintDefaultOutput()
+        {
+            commandOutputProvider.Information("Prevented successfully.");
+        }
+
+        public void PrintJsonOutput()
+        {
+            commandOutputProvider.Json(new
+            {
+                project.SpaceId,
+                Project = new { project.Id, project.Name },
+                Release = new { release.Id, release.Version, IsPreventedFromProgressing = true },
+                ReasonToPrevent
+            });
+        }
+    }
+}
