df57dd6474b17956e09609df6526afffcf11bb67
Andy Hsu <andy.hsu@octopus.com>
2020-03-11 15:54:30 +1000

    Removed unnecessary using and code

--- source/Octo/Program.cs
+++ source/Octo/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Octopus.Cli;
+﻿using Octopus.Cli;
 
 namespace Octo
 {
@@ -7,7 +6,6 @@ namespace Octo
     {
         public static int Main(string[] args)
         {
-            //args = new []{ "prevent-release-progression", "--server=http://localhost:8065", "--apiKey=API-T8EQW0TKAL2N10LDRS34GNHUS", "--project=TeamCity E2E Check", "-version=0.0.16", "--reason=123" };
             return new CliProgram().Execute(args);
         }
     }
