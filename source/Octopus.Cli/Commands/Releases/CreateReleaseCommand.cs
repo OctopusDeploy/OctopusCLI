OctopusDeploy/OctopusCLI

commit ed1b1d81bf35097b626fb8b114a95e99d1f8a70d

Make options typed, and include data type info in json help output (#65)
Instead of just assuming all our command line options are strings, this changes our options parsing to use generics, and uses the built in option parsing.

This means:
* we dont need to do safe parsing (which we were doing inconsistently anyway - quite a few commands failed if you passed invalid data)
* we parse enums properly (one of our tests was using dodgy data and we didn't know
* we now flag sensitive options
* our (json formatted) help output now includes data types and enum values

source/Octopus.Cli/Commands/Releases/CreateReleaseCommand.cs
@@ -31,21 +31,21 @@ namespace Octopus.Cli.Commands.Releases
             this.releasePlanBuilder = releasePlanBuilder;
 
             var options = Options.For("Release creation");
-            options.Add("project=", "Name or ID of the project", v => ProjectNameOrId = v);
-            options.Add("defaultPackageVersion=|packageVersion=", "Default version number of all packages to use for this release. Override per-package using --package.", versionResolver.Default);
-            options.Add("version=|releaseNumber=", "[Optional] Release number to use for the new release.", v => VersionNumber = v);
-            options.Add("channel=", "[Optional] Name or ID of the channel to use for the new release. Omit this argument to automatically select the best channel.", v => ChannelNameOrId = v);
-            options.Add("package=", "[Optional] Version number to use for a package in the release. Format: StepName:Version or PackageID:Version or StepName:PackageName:Version. StepName, PackageID, and PackageName can be replaced with an asterisk. An asterisk will be assumed for StepName, PackageID, or PackageName if they are omitted.", v => versionResolver.Add(v));
-            options.Add("packagesFolder=", "[Optional] A folder containing NuGet packages from which we should get versions.", v => {v.CheckForIllegalPathCharacters("packagesFolder"); versionResolver.AddFolder(v);});
-            options.Add("releaseNotes=", "[Optional] Release Notes for the new release. Styling with Markdown is supported.", v => ReleaseNotes = v);
-            options.Add("releaseNotesFile=", "[Optional] Path to a file that contains Release Notes for the new release. Supports Markdown files.", ReadReleaseNotesFromFile);
-            options.Add("ignoreExisting", "[Optional, Flag] Don't create this release if there is already one with the same version number.", v => IgnoreIfAlreadyExists = true);
-            options.Add("ignoreChannelRules", "[Optional, Flag] Create the release ignoring any version rules specified by the channel.", v => IgnoreChannelRules = true);
-            options.Add("packagePrerelease=", "[Optional] Pre-release for latest version of all packages to use for this release.", v => VersionPreReleaseTag = v);
-            options.Add("whatIf", "[Optional, Flag] Perform a dry run but don't actually create/deploy release.", v => WhatIf = true);
+            options.Add<string>("project=", "Name or ID of the project", v => ProjectNameOrId = v);
+            options.Add<string>("defaultPackageVersion=|packageVersion=", "Default version number of all packages to use for this release. Override per-package using --package.", versionResolver.Default);
+            options.Add<string>("version=|releaseNumber=", "[Optional] Release number to use for the new release.", v => VersionNumber = v);
+            options.Add<string>("channel=", "[Optional] Name or ID of the channel to use for the new release. Omit this argument to automatically select the best channel.", v => ChannelNameOrId = v);
+            options.Add<string>("package=", "[Optional] Version number to use for a package in the release. Format: StepName:Version or PackageID:Version or StepName:PackageName:Version. StepName, PackageID, and PackageName can be replaced with an asterisk. An asterisk will be assumed for StepName, PackageID, or PackageName if they are omitted.", v => versionResolver.Add(v));
+            options.Add<string>("packagesFolder=", "[Optional] A folder containing NuGet packages from which we should get versions.", v => {v.CheckForIllegalPathCharacters("packagesFolder"); versionResolver.AddFolder(v);});
+            options.Add<string>("releaseNotes=", "[Optional] Release Notes for the new release. Styling with Markdown is supported.", v => ReleaseNotes = v);
+            options.Add<string>("releaseNoteFile=", "[Optional] Path to a file that contains Release Notes for the new release. Supports Markdown files.", ReadReleaseNotesFromFile);
+            options.Add<bool>("ignoreExisting", "[Optional, Flag] Don't create this release if there is already one with the same version number.", v => IgnoreIfAlreadyExists = true);
+            options.Add<bool>("ignoreChannelRules", "[Optional, Flag] Create the release ignoring any version rules specified by the channel.", v => IgnoreChannelRules = true);
+            options.Add<string>("packagePrerelease=", "[Optional] Pre-release for latest version of all packages to use for this release.", v => VersionPreReleaseTag = v);
+            options.Add<bool>("whatIf", "[Optional, Flag] Perform a dry run but don't actually create/deploy release.", v => WhatIf = true);
 
             options = Options.For("Deployment");
-            options.Add("deployTo=", "[Optional] Name or ID of the environment to automatically deploy to, e.g., 'Production' or 'Environments-1'; specify this argument multiple times to deploy to multiple environments.", v => DeployToEnvironmentNamesOrIds.Add(v));
+            options.Add<string>("deployTo=", "[Optional] Name or ID of the environment to automatically deploy to, e.g., 'Production' or 'Environments-1'; specify this argument multiple times to deploy to multiple environments.", v => DeployToEnvironmentNamesOrIds.Add(v));
         }
 
         public string ChannelNameOrId { get; set; }

source/Octo.Tests/Commands/CompleteCommandFixture.cs
@@ -124,8 +124,8 @@ namespace Octo.Tests.Commands
         public TestCommand(ICommandOutputProvider commandOutputProvider) : base(commandOutputProvider)
         {
             var options = Options.For("Test group");
-            options.Add("apiKey", "api key", v => ApiKey = v);
-            options.Add("url", "url", v => Url = v);
+            options.Add<string>("apiKey", "api key", v => ApiKey = v);
+            options.Add<string>("url", "url", v => Url = v);
         }
 
         public string Url { get; set; }

source/Octo.Tests/Commands/DummyApiCommand.cs
@@ -14,7 +14,7 @@ namespace Octo.Tests.Commands
             : base(clientFactory, repositoryFactory, fileSystem, commandOutputProvider)
         {
             var options = Options.For("Dummy");
-            options.Add("pill=", "Red or Blue. Blue, the story ends. Red, stay in Wonderland and see how deep the rabbit hole goes.", v => pill = v);
+            options.Add<string>("pill=", "Red or Blue. Blue, the story ends. Red, stay in Wonderland and see how deep the rabbit hole goes.", v => pill = v);
             commandOutputProvider.Debug("Pill: " + pill);
         }
 

source/Octo.Tests/Commands/ListMachinesCommandFixture.cs
@@ -315,7 +315,7 @@ namespace Octo.Tests.Commands
         [Test]
         public async Task ShouldThrowIfUsingHealthStatusPre34()
         {
-            CommandLineArgs.Add("--health-status=Online");
+            CommandLineArgs.Add("--health-status=Healthy");
             (await Repository.LoadRootDocument()).Version = "3.1.0";
 
             Func<Task> exec = () => listMachinesCommand.Execute(CommandLineArgs.ToArray());

source/Octo.Tests/Commands/OptionsFixture.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using Octopus.Cli.Commands.Package;
 using Octopus.Cli.Infrastructure;
 
 namespace Octo.Tests.Commands
@@ -16,17 +18,25 @@ namespace Octo.Tests.Commands
         {
             var apiKey = string.Empty;
             var foo = string.Empty;
-            
-            var optionSet = new OptionSet()
-            {
-                {"apiKey=", "API key", v => apiKey = v},
-                {"foo=", "Foo", v => foo = v}
-            };
+
+            var optionSet = new OptionSet();
+            optionSet.Add<string>("apiKey=", "API key", v => apiKey = v);
+            optionSet.Add<string>("foo=", "Foo", v => foo = v);
 
             optionSet.Parse(new[] {parameter1, parameter2});
 
             Assert.That(apiKey, Is.EqualTo("abc123"));
             Assert.That(foo, Is.EqualTo("bar"));
+        }
+
+        [Test]
+        public void ShowsValidValuesForEnums()
+        {
+            var optionSet = new OptionSet();
+            optionSet.Add<PackageFormat>("packageFormat=", "Package Format", v => { });
+
+            var ex = Assert.Throws<CommandException>(() => optionSet.Parse(new[] {"--packageFormat", "invalidvalue"}));
+            Assert.That(ex.Message, Is.EqualTo("Could not convert string `invalidvalue' to type PackageFormat for option `--packageFormat'. Valid values are Zip, Nupkg and Nuget."));
         }
     }
 }

source/Octo.Tests/Commands/SpeakCommand.cs
@@ -16,7 +16,7 @@ namespace Octo.Tests.Commands
         public SpeakCommand(ICommandOutputProvider commandOutputProvider) : base(commandOutputProvider)
         {
             var options = Options.For("default");
-            options.Add("message=", m => { });
+            options.Add<string>("message=", m => { });
         }
     }
 }

source/Octo.Tests/Commands/SupportFormattedOutputFixture.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Newtonsoft.Json;
 using NUnit.Framework;
+using Octopus.Cli.Infrastructure;
 
 namespace Octo.Tests.Commands
 {
@@ -35,15 +36,16 @@ namespace Octo.Tests.Commands
         }
         
         [Test]
-        public async Task FormattedOutput_FormatInvalid()
+        public void FormattedOutput_FormatInvalid()
         {
             var command = new DummyApiCommandWithFormattedOutputSupport(ClientFactory, RepositoryFactory, FileSystem, CommandOutputProvider);
             CommandLineArgs.Add("--helpOutputFormat=blah");
 
-            await command.Execute(CommandLineArgs.ToArray()).ConfigureAwait(false);
+            var exception = Assert.ThrowsAsync<CommandException>(async () => await command.Execute(CommandLineArgs.ToArray()).ConfigureAwait(false));
 
-            command.PrintJsonOutputCalled.ShouldBeEquivalentTo(false);
-            command.PrintDefaultOutputCalled.ShouldBeEquivalentTo(true);
+            command.PrintJsonOutputCalled.Should().BeFalse();
+            command.PrintDefaultOutputCalled.Should().BeFalse();
+            exception.Message.Should().Be("Could not convert string `blah' to type OutputFormat for option `--helpOutputFormat'. Valid values are Default and Json.");
         }
 
         [Test]

source/Octopus.Cli/Commands/ApiCommand.cs
@@ -59,16 +59,16 @@ namespace Octopus.Cli.Commands
             this.FileSystem = fileSystem;
 
             var options = Options.For("Common options");
-            options.Add("server=", $"[Optional] The base URL for your Octopus Server, e.g., 'https://octopus.example.com/'. This URL can also be set in the {ServerUrlEnvVar} environment variable.", v => serverBaseUrl = v);
-            options.Add("apiKey=", $"[Optional] Your API key. Get this from the user profile page. Your must provide an apiKey or username and password. If the guest account is enabled, a key of API-GUEST can be used. This key can also be set in the {ApiKeyEnvVar} environment variable.", v => apiKey = v);
-            options.Add("user=", $"[Optional] Username to use when authenticating with the server. Your must provide an apiKey or username and password. This Username can also be set in the {UsernameEnvVar} environment variable.", v => username = v);
-            options.Add("pass=", $"[Optional] Password to use when authenticating with the server. This Password can also be set in the {PasswordEnvVar} environment variable.", v => password = v);
+            options.Add<string>("server=", $"[Optional] The base URL for your Octopus Server, e.g., 'https://octopus.example.com/'. This URL can also be set in the {ServerUrlEnvVar} environment variable.", v => serverBaseUrl = v);
+            options.Add<string>("apiKey=", $"[Optional] Your API key. Get this from the user profile page. Your must provide an apiKey or username and password. If the guest account is enabled, a key of API-GUEST can be used. This key can also be set in the {ApiKeyEnvVar} environment variable.", v => apiKey = v, sensitive: true);
+            options.Add<string>("user=", $"[Optional] Username to use when authenticating with the server. Your must provide an apiKey or username and password. This Username can also be set in the {UsernameEnvVar} environment variable.", v => username = v);
+            options.Add<string>("pass=", $"[Optional] Password to use when authenticating with the server. This Password can also be set in the {PasswordEnvVar} environment variable.", v => password = v, sensitive: true);
 
-            options.Add("configFile=", "[Optional] Text file of default values, with one 'key = value' per line.", v => ReadAdditionalInputsFromConfigurationFile(v));
-            options.Add("debug", "[Optional] Enable debug logging", v => enableDebugging = true);
-            options.Add("ignoreSslErrors", "[Optional] Set this flag if your Octopus Server uses HTTPS but the certificate is not trusted on this machine. Any certificate errors will be ignored. WARNING: this option may create a security vulnerability.", v => ignoreSslErrors = true);
-            options.Add("enableServiceMessages", "[Optional] Enable TeamCity or Team Foundation Build service messages when logging.", v => commandOutputProvider.EnableServiceMessages());
-            options.Add("timeout=", $"[Optional] Timeout in seconds for network operations. Default is {ApiConstants.DefaultClientRequestTimeout/1000}.", v =>
+            options.Add<string>("configFile=", "[Optional] Text file of default values, with one 'key = value' per line.", v => ReadAdditionalInputsFromConfigurationFile(v));
+            options.Add<bool>("debug", "[Optional] Enable debug logging", v => enableDebugging = true);
+            options.Add<bool>("ignoreSslErrors", "[Optional] Set this flag if your Octopus Server uses HTTPS but the certificate is not trusted on this machine. Any certificate errors will be ignored. WARNING: this option may create a security vulnerability.", v => ignoreSslErrors = true);
+            options.Add<bool>("enableServiceMessages", "[Optional] Enable TeamCity or Team Foundation Build service messages when logging.", v => commandOutputProvider.EnableServiceMessages());
+            options.Add<string>("timeout=", $"[Optional] Timeout in seconds for network operations. Default is {ApiConstants.DefaultClientRequestTimeout/1000}.", v =>
             {
                 if (int.TryParse(v, out var parsedInt))
                     clientOptions.Timeout = TimeSpan.FromSeconds(parsedInt);
@@ -77,18 +77,12 @@ namespace Octopus.Cli.Commands
                 else
                     throw new CommandException($"Unable to parse '{v}' as a timespan or an integer.");
             });
-            options.Add("proxy=", $"[Optional] The URL of the proxy to use, e.g., 'https://proxy.example.com'.", v => clientOptions.Proxy = v);
-            options.Add("proxyUser=", $"[Optional] The username for the proxy.", v => clientOptions.ProxyUsername = v);
-            options.Add("proxyPass=", $"[Optional] The password for the proxy. If both the username and password are omitted and proxyAddress is specified, the default credentials are used. ", v => clientOptions.ProxyPassword = v);
-            options.Add("space=", $"[Optional] The name or ID of a space within which this command will be executed. The default space will be used if it is omitted. ", v => spaceNameOrId = v);
+            options.Add<string>("proxy=", $"[Optional] The URL of the proxy to use, e.g., 'https://proxy.example.com'.", v => clientOptions.Proxy = v);
+            options.Add<string>("proxyUser=", $"[Optional] The username for the proxy.", v => clientOptions.ProxyUsername = v);
+            options.Add<string>("proxyPass=", $"[Optional] The password for the proxy. If both the username and password are omitted and proxyAddress is specified, the default credentials are used.", v => clientOptions.ProxyPassword = v, sensitive: true);
+            options.Add<string>("space=", $"[Optional] The name or ID of a space within which this command will be executed. The default space will be used if it is omitted.", v => spaceNameOrId = v);
 #if NETFRAMEWORK
-            options.Add("keepalive=", "[Optional] How frequently (in seconds) to send a TCP keepalive packet.", input =>
-            {
-                if (int.TryParse(input, out var parsedInt))
-                    keepAlive = parsedInt * 1000;
-                else
-                    throw new CommandException($"Unable to parse '{input}' as an integer.");
-            });
+            options.Add<int>("keepalive=", "[Optional] How frequently (in seconds) to send a TCP keepalive packet.", input => keepAlive = input * 1000);
 #endif
             options.AddLogLevelOptions();
         }
@@ -272,32 +266,6 @@ namespace Octopus.Cli.Commands
                 {
                     formattedOutputInstance.PrintDefaultOutput();
                 }
-            }
-        }
-
-        static NetworkCredential ParseCredentials(string username, string password)
-        {
-            if (string.IsNullOrWhiteSpace(username))
-                return CredentialCache.DefaultNetworkCredentials;
-
-            var split = username.Split('\\');
-            if (split.Length == 2)
-            {
-                var domain = split.First();
-                username = split.Last();
-
-                return new NetworkCredential(username, password, domain);
-            }
-
-            return new NetworkCredential(username, password);
-        }
-
-        protected void SetFlagState(string input, ref bool? setter)
-        {
-            bool tempBool;
-            if (bool.TryParse(input, out tempBool))
-            {
-                setter = tempBool;
             }
         }
 

commit 9e91183c2ecd869e23ada1bf1417996447cc109e

Add an alias for release notes file on create-release
To fix a mistake introduced in https://github.com/OctopusDeploy/OctopusCLI/commit/ed1b1d81bf35097b626fb8b114a95e99d1f8a70d#diff-91a6a333eea8955a7cf22f9b3bf30a49R41.
Fixes https://github.com/OctopusDeploy/OctopusCLI/issues/70

source/Octopus.Cli/Commands/Releases/CreateReleaseCommand.cs
@@ -38,7 +38,7 @@ namespace Octopus.Cli.Commands.Releases
             options.Add<string>("package=", "[Optional] Version number to use for a package in the release. Format: StepName:Version or PackageID:Version or StepName:PackageName:Version. StepName, PackageID, and PackageName can be replaced with an asterisk. An asterisk will be assumed for StepName, PackageID, or PackageName if they are omitted.", v => versionResolver.Add(v));
             options.Add<string>("packagesFolder=", "[Optional] A folder containing NuGet packages from which we should get versions.", v => {v.CheckForIllegalPathCharacters("packagesFolder"); versionResolver.AddFolder(v);});
             options.Add<string>("releaseNotes=", "[Optional] Release Notes for the new release. Styling with Markdown is supported.", v => ReleaseNotes = v);
-            options.Add<string>("releaseNoteFile=", "[Optional] Path to a file that contains Release Notes for the new release. Supports Markdown files.", ReadReleaseNotesFromFile);
+            options.Add<string>("releaseNoteFile=|releaseNotesFile=", "[Optional] Path to a file that contains Release Notes for the new release. Supports Markdown files.", ReadReleaseNotesFromFile);
             options.Add<bool>("ignoreExisting", "[Optional, Flag] Don't create this release if there is already one with the same version number.", v => IgnoreIfAlreadyExists = true);
             options.Add<bool>("ignoreChannelRules", "[Optional, Flag] Create the release ignoring any version rules specified by the channel.", v => IgnoreChannelRules = true);
             options.Add<string>("packagePrerelease=", "[Optional] Pre-release for latest version of all packages to use for this release.", v => VersionPreReleaseTag = v);