0945b2083e2a27a1d404c1501939b7b3dedd135c
David Young <thedewi@users.noreply.github.com>
2020-06-18 10:35:31 +1000

    Extract octo-specific parts of test-linux-packages.sh

---
+++ BuildAssets/test-linux-packages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Test that .deb and .rpm packages in the working directory install an octo command that can list-environments.
+
+source "$(dirname "$0")/../linux-package-feeds/test-linux-packages.sh"
--- linux-package-feeds/test-linux-packages.sh
+++ linux-package-feeds/test-linux-packages.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # Test that .deb and .rpm packages in the working directory install an octo command that can list-environments.
 
-which docker >/dev/null || {
-  echo 'This script requires docker.' >&2
-  exit 1
-}
 if [[ -z "$OCTOPUS_CLI_SERVER" || -z "$OCTOPUS_CLI_API_KEY" || -z "$OCTOPUS_SPACE" || -z "$OCTOPUS_EXPECT_ENV" ]]; then
   echo -e 'This script requires the environment variables OCTOPUS_CLI_SERVER, OCTOPUS_CLI_API_KEY, OCTOPUS_SPACE, and'\
     '\nOCTOPUS_EXPECT_ENV - specifying an Octopus server for testing "list-environments", an API key to access it, the'\
@@ -17,14 +13,13 @@ if [[ -z "$TEST_QUICK" && ( -z "$REDHAT_SUBSCRIPTION_USERNAME" || -z "$REDHAT_SU
   exit 1
 fi
 
-TEST_SH='
-  # Test octo
-  octo version || exit
-  OCTO_RESULT="$(octo list-environments --space="$OCTOPUS_SPACE")" || { echo "$OCTO_RESULT"; exit 1; }
-  echo "$OCTO_RESULT" | grep "$OCTOPUS_EXPECT_ENV" || { echo "Expected environment not found: $OCTOPUS_EXPECT_ENV." >&2; exit 1; }
-'
+which docker >/dev/null || {
+  echo 'This script requires docker.' >&2
+  exit 1
+}
 
-TEST_DEB_SH='
+TEST_SH='
+if which dpkg > /dev/null; then
   # Configure apt
   export DEBIAN_FRONTEND=noninteractive
   apt-get update --quiet 2 || exit
@@ -34,13 +29,7 @@ TEST_DEB_SH='
   dpkg -i /pkgs/octopuscli*.deb >/dev/null 2>&1 # Silenced and expected to fail due to missing deps, which apt can fix
   apt-get --no-install-recommends --yes --fix-broken install 2>&1 >/dev/null || exit
   apt-get --no-install-recommends --yes install ca-certificates >/dev/null || exit
-'"$TEST_SH"
-
-TEST_RPM_SH='
-if [[ $(. /etc/os-release && echo $ID) != "rhel" ]]; then
-  # Install octo
-  yum --quiet --assumeyes localinstall /pkgs/octopuscli*.rpm 2>&1 || exit
-else
+elif [[ $(. /etc/os-release && echo $ID) == "rhel" ]]; then
   # Install octo, but first:
   #   - Register with Red Hat to enable yum
   #   - Install yum-plugin-ovl to reduce chance of "Rpmdb checksum is invalid"
@@ -61,8 +50,16 @@ else
     echo "$ERR_OUT" >&2
     exit $STATUS
   fi
+else
+  # Install octo
+  yum --quiet --assumeyes localinstall /pkgs/octopuscli*.rpm 2>&1 || exit
 fi
-'"$TEST_SH"
+
+# Test octo
+octo version || exit
+OCTO_RESULT="$(octo list-environments --space="$OCTOPUS_SPACE")" || { echo "$OCTO_RESULT"; exit 1; }
+echo "$OCTO_RESULT" | grep "$OCTOPUS_EXPECT_ENV" || { echo "Expected environment not found: $OCTOPUS_EXPECT_ENV." >&2; exit 1; }
+'
 
 test_in_docker() {
   echo "== Testing in '$1' =="
@@ -72,20 +69,20 @@ test_in_docker() {
     "$1" bash -c "$2" || exit
 }
 
-test_in_docker debian:stable-slim "$TEST_DEB_SH"
-# ZZDY NOSHIP test_in_docker ubuntu:latest "$TEST_DEB_SH"
-test_in_docker centos:latest "$TEST_RPM_SH"
+test_in_docker debian:stable-slim "$TEST_SH"
+# ZZDY NOSHIP test_in_docker ubuntu:latest "$TEST_SH"
+test_in_docker centos:latest "$TEST_SH"
 # ZZDY NOSHIP if [ -n "$TEST_QUICK" ]; then
   echo "TEST_QUICK is enabled. Skipping the remaining distros."
   exit 0
 # ZZDY NOSHIP fi
-test_in_docker fedora:latest "$TEST_RPM_SH"
-test_in_docker debian:oldstable-slim "$TEST_DEB_SH"
-test_in_docker ubuntu:rolling "$TEST_DEB_SH"
-test_in_docker linuxmintd/mint19.3-amd64 "$TEST_DEB_SH"
-test_in_docker centos:7 "$TEST_RPM_SH"
-test_in_docker ubuntu:xenial "$TEST_DEB_SH"
-test_in_docker roboxes/rhel8 "$TEST_RPM_SH"
-test_in_docker debian:oldoldstable-slim "$TEST_DEB_SH"
-test_in_docker ubuntu:trusty "$TEST_DEB_SH"
-test_in_docker roboxes/rhel7 "$TEST_RPM_SH"
+test_in_docker fedora:latest "$TEST_SH"
+test_in_docker debian:oldstable-slim "$TEST_SH"
+test_in_docker ubuntu:rolling "$TEST_SH"
+test_in_docker linuxmintd/mint19.3-amd64 "$TEST_SH"
+test_in_docker centos:7 "$TEST_SH"
+test_in_docker ubuntu:xenial "$TEST_SH"
+test_in_docker roboxes/rhel8 "$TEST_SH"
+test_in_docker debian:oldoldstable-slim "$TEST_SH"
+test_in_docker ubuntu:trusty "$TEST_SH"
+test_in_docker roboxes/rhel7 "$TEST_SH"
