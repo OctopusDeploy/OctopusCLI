bee7c3acfbea56101e7903a2320aeac19a0e1df3
David Young <thedewi@users.noreply.github.com>
2020-06-22 13:37:31 +1000

    Extract octo-specific parts of test-linux-packages.sh

--- BuildAssets/test-octo-packages.sh
+++ BuildAssets/test-octo-packages.sh
@@ -12,13 +12,22 @@ if [[ -z "$TEST_QUICK" && ( -z "$REDHAT_SUBSCRIPTION_USERNAME" || -z "$REDHAT_SU
     '\na test system, or TEST_QUICK set to any value to skip Red Hat and some other distributions.' >&2
   exit 1
 fi
+which docker >/dev/null || {
+  echo 'This script requires docker.' >&2
+  exit 1
+}
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-TEST_SCRIPT_FILE="$SCRIPT_DIR/test-octo-package.sh"
-DOCKER_OPTS=(
-  --volume "$SCRIPT_DIR/../linux-package-feeds:/linux-package-feeds"
-  --env OCTOPUS_CLI_SERVER --env OCTOPUS_CLI_API_KEY --env OCTOPUS_SPACE --env OCTOPUS_EXPECT_ENV \
-  --env REDHAT_SUBSCRIPTION_USERNAME --env REDHAT_SUBSCRIPTION_PASSWORD \
-)
-source "$SCRIPT_DIR/../linux-package-feeds/test-linux-packages.sh"
+for DOCKER_IMAGE in $(cat "$SCRIPT_DIR/../linux-package-feeds/test-env-docker-images.conf" | grep -o '^[^#]*')
+do
+  echo "== Testing in '$DOCKER_IMAGE' =="
+  docker pull "$DOCKER_IMAGE" >/dev/null || exit
+  docker run --rm \
+    --hostname "testpkgs$RANDOM" \
+    --volume "$(pwd):/working" --volume "$SCRIPT_DIR/test-octo-package.sh:/test-octo-package.sh" \
+    --volume "$SCRIPT_DIR/../linux-package-feeds:/linux-package-feeds"
+    --env OCTOPUS_CLI_SERVER --env OCTOPUS_CLI_API_KEY --env OCTOPUS_SPACE --env OCTOPUS_EXPECT_ENV \
+    --env REDHAT_SUBSCRIPTION_USERNAME --env REDHAT_SUBSCRIPTION_PASSWORD \
+    "$DOCKER_IMAGE" bash -c 'cd /working && bash /test-octo-package.sh' || exit
+done
