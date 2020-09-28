7c6c66057c4be4f4937a4b71f84b06b5b95bdade
Shane Gill <tothegills@users.noreply.github.com>
2020-09-29 08:44:44 +1000

    Create release from git reference (#99)
    * Create release from git reference
    
    * Add assertion to test
    
    * Update Octpus.Client to get most recent config-as-code changes
    
    * Display commit and ref after creating release
    
    * Check for null for compatability with old servers

--- source/Octopus.Cli/Commands/Releases/IReleasePlanBuilder.cs
+++ source/Octopus.Cli/Commands/Releases/IReleasePlanBuilder.cs
@@ -6,6 +6,6 @@ namespace Octopus.Cli.Commands.Releases
 {
     public interface IReleasePlanBuilder
     {
-        Task<ReleasePlan> Build(IOctopusAsyncRepository repository, ProjectResource project, ChannelResource channel, string versionPreReleaseTag);
+        Task<ReleasePlan> Build(IOctopusAsyncRepository repository, ProjectResource project, ChannelResource channel, string versionPreReleaseTag, string gitReference);
     }
 }
