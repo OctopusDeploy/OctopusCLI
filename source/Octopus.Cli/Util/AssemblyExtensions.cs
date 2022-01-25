806602c1d1e0a8ba4779a868da3db5fe0db66b31
John Simons <john.simons@octopus.com>
2022-01-25 14:56:00 +1000

    Not used

--- source/Octopus.Cli/Util/AssemblyExtensions.cs
+++ source/Octopus.Cli/Util/AssemblyExtensions.cs
@@ -1,28 +1,10 @@
 ﻿using System;
-using System.IO;
 using System.Reflection;
 
 namespace Octopus.Cli.Util
 {
     public static class AssemblyExtensions
     {
-        public static string FullLocalPath(this Assembly assembly)
-        {
-#if NETFRAMEWORK
-            //SYSLIB0012: 'Assembly.CodeBase' is obsolete: 'Assembly.CodeBase and Assembly.EscapedCodeBase are
-            ////only included for .NET Framework compatibility. Use Assembly.Location instead.'
-            
-            var codeBase = assembly.CodeBase;
-            var uri = new UriBuilder(codeBase);
-            var root = Uri.UnescapeDataString(uri.Path);
-            root = root.Replace('/', Path.DirectorySeparatorChar);
-            return root;
-#else
-            return assembly.Location;
-#endif
-            
-        }
-
         public static string GetInformationalVersion(this Type type)
         {
             return type.GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
